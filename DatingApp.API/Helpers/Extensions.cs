143209407f39f0e5f49e158dc15aa73b9d596a91
jambis <artur.kozlowski@gmail.com>
2019-07-01 11:24:50 +0200

    End of section 5

---
+++ DatingApp.API/Helpers/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Helpers
+{
+    public static class Extensions
+    {
+        // to add a header required by cors
+        public static void AddApplicationError(this HttpResponse response, string message)
+        {
+            response.Headers.Add("Application-Error", message);
+            response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
+            response.Headers.Add("Access-Control-Allow-Origin", "*");
+        }
+    }
+}
